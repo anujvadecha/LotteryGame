51f3533e670726a2e81893ddd5a14fbffb15be70
Vadecha <anuj.vadecha@PFIN-MI-C02DF8A1P3XY.local>
2021-04-24 16:39:20 +0530

    MODEL CHANGES FOR BALANCE POINTS

--- GameMasterApp/models.py
+++ GameMasterApp/models.py
@@ -3,31 +3,34 @@ from django.db import models
 # Create your models here.
 from django.contrib.auth.models import AbstractUser
 from base.models import *
+from base.utils import random_string_generator, unique_transaction_id_generator
 
 
 class User(AbstractUser):
-    phone_number=models.CharField(default="",max_length=256,blank=True,null=True)
+    phone_number = models.CharField(default="", max_length=256, blank=True, null=True)
     address = models.TextField(default="", blank=True, null=True)
+    balance_points = models.IntegerField(default=0, null=False)
 
     def name(self):
         return self.first_name + ' ' + self.last_name
 
+
 class Players(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    balance_points = models.IntegerField(default=100, null=False)
+
 
 class Region(BaseModel):
     name = models.CharField(unique=True, default="", max_length=256, null=False, blank=False)
 
+
 class Agent(BaseModel):
-    user = models.ForeignKey(User,blank=False,null=False,on_delete=models.CASCADE, related_name="agent_user")
+    user = models.ForeignKey(User, blank=False, null=False, on_delete=models.CASCADE, related_name="agent_user")
     company_name = models.CharField(max_length=255, default="")
     # Enabled flag no need since this will be controled by
     commission_percent = models.FloatField(default=0)
     agent_created_by = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True,
                                          related_name="agent_created_by")
     region = models.ForeignKey(Region, null=True, blank=True, on_delete=models.SET_NULL)
-    balance_points = models.IntegerField(default=0, null=False)
 
 
 class RegionalManager(BaseModel):
@@ -55,14 +58,16 @@ class Lottery(BaseModel):
     active = models.BooleanField(default=True)
     completed = models.BooleanField(default=False)
 
+
 class TicketID(BaseModel):
+    ticket_id = models.CharField(primary_key=True, default=unique_transaction_id_generator, max_length=50)
     user = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE)
     ticket_set = models.ManyToManyField(Ticket, blank=True)
     total_price = models.IntegerField(default=0, blank=False, null=False)
     total_quantity = models.IntegerField(default=0, blank=False, null=False)
     is_completed = models.BooleanField(default=False)
-    lottery = models.ForeignKey(Lottery,on_delete=models.SET_NULL,null=True,blank=True)
-    returns = models.IntegerField(default=0,null=True,blank=True)
+    lottery = models.ForeignKey(Lottery, on_delete=models.SET_NULL, null=True, blank=True)
+    returns = models.IntegerField(default=0, null=True, blank=True)
 
     def save(self, *args, **kwargs):
         try:
@@ -82,7 +87,6 @@ class TicketID(BaseModel):
             super(TicketID, self).save(*args, **kwargs)
 
 
-
 class UserLedgerHistory(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     credit = models.IntegerField(default=0)
@@ -96,4 +100,3 @@ class Admin(BaseModel):
     regional_managers = models.ManyToManyField(RegionalManager, blank=True)
     agents = models.ManyToManyField(Agent, blank=True)
     players = models.ManyToManyField(Players, blank=True)
-
---
+++ base/utils.py
@@ -0,0 +1,11 @@
+import string
+import random
+
+
+def random_string_generator(size=13, chars =  string.digits):
+    return 'LT'+''.join(random.choice(chars) for _ in range(size))
+
+def unique_transaction_id_generator():
+    transaction_new_id = random_string_generator(13)
+    # t_id_exists = Klass.objects.filter(transaction_id=transaction_new_id).exists()
+    return transaction_new_id
