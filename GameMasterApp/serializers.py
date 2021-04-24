2354fee2a15ec3ceb16cbe2d03fc0b5020338620
Vadecha <anuj.vadecha@PFIN-MI-C02DF8A1P3XY.local>
2021-04-24 17:13:14 +0530

    winners json parse

--- GameMasterApp/serializers.py
+++ GameMasterApp/serializers.py
@@ -1,3 +1,4 @@
+import json
 from datetime import timedelta
 
 from rest_framework import serializers
@@ -7,8 +8,12 @@ from GameMasterApp.models import Lottery
 
 class LotterySerializer(serializers.ModelSerializer):
     time_ist = serializers.SerializerMethodField()
+    winners = serializers.SerializerMethodField()
+
     def get_time_ist(self,obj):
         return obj.time+timedelta(hours=5,minutes=30)
+    def get_winners(self,obj):
+        return json.loads(obj.winners)
     class Meta:
         model=Lottery
         fields=["id","winners","active","completed","time_ist"]
