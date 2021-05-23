29172027a2d966d62672938c8f681750e6a0a6d1
Nayan Jain <nayanjain@Nayans-MacBook-Air.local>
2021-05-23 18:51:49 +0530

    UI changes, ticket cancellation

--- GameMasterApp/start_scheduler.py
+++ GameMasterApp/start_scheduler.py
@@ -2,15 +2,15 @@ import time
 import subprocess
 import json
 
-#
-# while True:
-#     cmd = open("scheduler.json", "r")
-#     subprocess.run(json.loads(cmd.read())["command"], shell=True)
-#     time.sleep(30)
-
-
-from GameMasterApp.assign_task import assign_lottery_timings
 
 while True:
-    assign_lottery_timings()
-    time.sleep(30)
+    cmd = open("scheduler.json", "r")
+    subprocess.run(json.loads(cmd.read())["command"], shell=True)
+    time.sleep(300)
+
+#
+# from GameMasterApp.assign_task import assign_lottery_timings
+#
+# while True:
+#     assign_lottery_timings()
+#     time.sleep(300)
