7d99686495c421e7a72f8b5da164ab46d4f34fc0
Vadecha <anuj.vadecha@ip-192-168-0-100.ap-south-1.compute.internal>
2021-06-10 23:53:35 +0530

    changes

--- GameVue/src/common/utils.js
+++ GameVue/src/common/utils.js
@@ -87,8 +87,7 @@ function get_current_date(){
     }
 
 function isToday(someDate) {
-
-  const today = new Date()
+  const today = new Date();
   return someDate.getDate() == today.getDate() &&
     someDate.getMonth() == today.getMonth() &&
     someDate.getFullYear() == today.getFullYear()
