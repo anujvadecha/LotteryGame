2d57cf7c059bcaa5862adea8167a40c539beec10
Vadecha <anuj.vadecha@ip-192-168-0-100.ap-south-1.compute.internal>
2021-06-14 20:40:22 +0530

    changes

--- GameMasterApp/assign_task.py
+++ GameMasterApp/assign_task.py
@@ -15,23 +15,26 @@ def assign_lottery_timings():
         if lottery_obj:
             # validate_lottery_conditions(lottery=lottery_obj)
             closest_time = lottery_obj.time
+            print(f"closest_time {closest_time} ")
             difference_for_next_lottery = (closest_time - current_time).total_seconds()
             winner_dict = {"A": "", "B": "", "C": "", "D": "", "E": "", "F": "", "G": "", "H": "", "I": "", "J": ""}
+            print(difference_for_next_lottery)
             if difference_for_next_lottery <= time_difference and difference_for_next_lottery > 0:
                 # and lottery_obj.winners == "{}":
+                print(" running lottery ")
                 for key, value in winner_dict.items():
+                    print("assigning winner")
                     winner_dict[key] = random.randint(0, 99)
-                # winner_dict = {"A": "5", "B": "5", "C": "0", "D": "0", "E": "0", "F": "0", "G": "0", "H": "0", "I": "0", "J": "0"}
                 lottery_obj.winners = json.dumps(winner_dict)
                 lottery_obj.completed = True
                 lottery_obj.save()
                 for key, value in winner_dict.items():
-                    for items in Ticket.objects.filter(lottery = lottery_obj, set_ticket=str(key) + str(value)):
-                        #total_winning = items.total_price() * 9
-                        ticket_id = TicketID.objects.filter(ticket_set__in = [items],cancelled=False,is_completed=False,lottery=lottery_obj)
+                    for items in Ticket.objects.filter(lottery=lottery_obj, set_ticket=str(key) + str(value)):
+                        total_winning = items.total_price() * 90
+                        ticket_id = TicketID.objects.filter(ticket_set__in=[items], cancelled=False)
                         if ticket_id:
                             ticket_id = ticket_id.first()
-                            ticket_id.increase_on_win(value)
+                            ticket_id.increase_inflow(total_winning)
                             ticket_id.save()
                 lottery_obj.save()
     except Exception as e:
