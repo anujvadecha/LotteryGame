c7f7c42ce6afd81f9c040c843ca4cf7df5e4ee92
Ubuntu <ubuntu@ip-172-31-41-115.us-east-2.compute.internal>
2021-06-18 05:04:34 +0000

    revert commit

--- GameVue/src/components/Footer.vue
+++ GameVue/src/components/Footer.vue
@@ -116,10 +116,8 @@ name: "Footer",
       selection:this.$store.state.selectionState
     }
     const  store=this.$store;
-    // const router = this.$router
     const q=this.$q;
     place_order(order).then(res=>{
-      console.log(res)
       if(res.status_code===200) {
         store.dispatch('update_balance_points',res.balance_points)
         var tickets_booked = res.tickets.map(ticket=>{
@@ -138,14 +136,10 @@ name: "Footer",
         if(document.getElementById("all_set_checkbox").checked){
           document.getElementById("all_set_checkbox").click()
         }}
-        catch (e){}
-      // router.push({
-      //   path: '/SelectionTable',
-      //   name:'SelectionTable',
-      //   params: {
-      //     set: 'A'
-      //   }
-      // })
+        catch (e){
+
+        }
+
 
         return ticket.ticket_id})
         res.tickets.map(ticket => {
